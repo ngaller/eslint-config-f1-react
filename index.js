f03acd6e3353161de05bdc70fa30f2c0801673d7
Nicolas Galler <ngaller@gmail.com>
2017-05-12 10:04:43 -0400

    package

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "ecmaFeatures": {
+    "jsx": true,
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "react/jsx-uses-react": 2,
+    "react/jsx-uses-vars": 2,
+    "react/react-in-jsx-scope": 2,
+  },
+  "env": {
+    "browser": true
+  },
+  "extends": "f1"
+}
